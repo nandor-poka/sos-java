28742f63b4e1120a547b310ef39294260df17e1f
np <np@gerhardt.io>
2020-05-06 16:28:52 +0200

    added nice session info, added minimal preview implementation

--- src/sos_java/kernel.py
+++ src/sos_java/kernel.py
@@ -8,7 +8,7 @@ _javaNumericTypes = ['Integer', 'Long', 'Float', 'Double', 'Short', 'Byte']
 _javaStringTypes = ['String', 'Char']
 
 # Converting to Java. Returns [<java type as string>, value]
-def __convert_to_java(var_from_sos):
+def _convert_to_java(var_from_sos):
     if var_from_sos is None:
         return ['Void','NULL']
     if isinstance(var_from_sos, bool):
@@ -31,7 +31,7 @@ def __convert_to_java(var_from_sos):
         return ['double', repr(var_from_sos)]
     return None
 
-def __get_java_type(self, javaVar):
+def _get_java_type_and_value(self, javaVar):
     try:
         javaVarType = self.sos_kernel.get_response(f'System.out.println(((Object){javaVar}).getClass().getSimpleName());', ('stream',), 
         name=('stdout','stderr') )[0][1]['text']
@@ -41,14 +41,22 @@ def __get_java_type(self, javaVar):
     except Exception as e:
         self.sos_kernel.warn(f'Exception occured when determining Java type of {javaVar}. {e.__str__()}')
 
-def __convert_from_java_to_Python( self, javaVar):
-    javaVarTypeAndValue = __get_java_type(self, javaVar)
+def _convert_from_java_to_Python(self, javaVar):
+    javaVarTypeAndValue = _get_java_type_and_value(self, javaVar)
     if javaVarTypeAndValue["type"] in _javaNumericTypes:
         return f'{javaVar} = {javaVarTypeAndValue["value"]}\n'
     if javaVarTypeAndValue["type"] in _javaStringTypes:
         return f'{javaVar} = "{javaVarTypeAndValue["value"]}"\n'
+        
 
-def __readSettings():
+def _convert_from_java_to_SoS(self, javaVar):
+    javaVarTypeAndValue = _get_java_type_and_value(self, javaVar)
+    if javaVarTypeAndValue["type"] in _javaNumericTypes:
+       return f'{javaVarTypeAndValue["value"]}'
+    if javaVarTypeAndValue["type"] in _javaStringTypes:
+       return f'"{javaVarTypeAndValue["value"]}"'
+
+def _readSettings():
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     settingsFilePath = os.path.join(__location__, 'settings.json')
     settings = None
@@ -61,11 +69,12 @@ def __readSettings():
     return json.loads(settings, encoding='utf-8')
         
 class sos_java:
-    settings = __readSettings()
+    settings = _readSettings()
     background_color = settings["color"] if settings else '#ffaba3'
     allow_overwrite = settings["allowOverwrite"] if settings else False
     supported_kernels = {'Java': ['java']}
-    options = {'assignment_pattern': r'^\s*([A-Za-z0-9\.]+)\s*(=).*$'}
+    #options = {'assignment_pattern': r'^\s*([A-Za-z0-9\.]+)\s*(=).*$'}
+    options = {}
     cd_command = ''
     __version__ = __version__
     java_vars = dict()
@@ -88,7 +97,7 @@ class sos_java:
             if changed:
                  self.sos_kernel.warn(f'Variable "{name}" from SoS to kernel {self.kernel_name} has been renamed to "{newname}" to follow language conventions')
             
-            type_and_value = __convert_to_java(env.sos_dict[name]) 
+            type_and_value = _convert_to_java(env.sos_dict[name]) 
             if type_and_value is None:
                 self.sos_kernel.warn(f'Unsupported datatype {repr(env.sos_dict[name])} by {self.kernel_name}')
             else:
@@ -106,20 +115,36 @@ class sos_java:
                     sos_java.java_vars[newname] = type_and_value[0]
 
     def put_vars(self, items, to_kernel=None):
+        print (items)
+        if not items:
+            return None
         if to_kernel in ['python3', 'Python3']:
             pythonCmd = ''
             try:
                 for varName in items:
-                    pythonCmd += __convert_from_java_to_Python(self, varName )
+                    pythonCmd += _convert_from_java_to_Python(self, varName )
             except Exception as e:
-                self.sos_kernel.warn(f'Exception occurred when transferring variables from Java to {to_kernel}. {e.__str__()}')
+                self.sos_kernel.warn(f'Exception occurred when transferring {varName} from Java to {to_kernel}. {e.__str__()}')
             return pythonCmd
+        elif to_kernel == None:
+            dictToSos = dict()
+            try:
+                for varName in items:
+                    dictToSos[varName] += _convert_from_java_to_Python(self, varName )
+            except Exception as e:
+                self.sos_kernel.warn(f'Exception occurred when transferring {varName} from Java to {to_kernel}. {e.__str__()}')
+            return dictToSos
             
     def expand(self, text, sigil):
         return None
     
     def preview(self, item):
-        return None
+        javaVarTypeAndValue = _get_java_type_and_value(self,item)
+        return (javaVarTypeAndValue["type"], javaVarTypeAndValue["value"])
 
     def sessioninfo(self):
-        return f'Kernel: {self.kernel_name},color: {self.background_color} settings: {self.settings}'
+        native_java = ''
+        for prop in ['java.vm.name','java.runtime.version']:
+            native_java += self.sos_kernel.get_response(f'System.out.println(System.getProperty("{prop}"));', ('stream',), 
+            name=('stdout', ' stderr') )[0][1]["text"] +" "
+        return [('Native Java', native_java), ("JupyterLab sos-java settings",f'{self.settings}')]
