4d0f3245f44a57d51de414db44fb118440dd8a65
np <np@gerhardt.io>
2020-05-08 11:35:01 +0200

    added recursive conversion of python (sos) tuple to Java Set<T>

--- src/sos_java/kernel.py
+++ src/sos_java/kernel.py
@@ -4,22 +4,54 @@ import math, os
 import numpy as np
 import json
 init_statements = ''
+
 # global variables to be used for Java variable conversion
 _javaNumericTypes = ['Integer', 'Long', 'Float', 'Double', 'Short', 'Byte']
 _javaStringTypes = ['String', 'Char']
 
-def _convert_None_to_Java(var_from_sos):
+# dict used as 'switch' to convert primitive numeric Java types to ther boxing class
+_Java_primitive_to_BoxingClass = {
+    'int':'Integer',
+    'float': 'Float',
+    'double': 'Double',
+    'byte': 'Byte'
+}
+
+def _check_type_homogeneity_in_collection(var_from_sos):
+    type_of_first_element = type(var_from_sos[0])
+    return all(isinstance(element, type_of_first_element) for element in var_from_sos)
+
+def _convert_None_to_Java(self, var_from_sos, varName):
      return ['Void','NULL']
-def _convert_Integers_to_Java(var_from_sos):
+
+def _convert_Integers_to_Java(self, var_from_sos, varName):
     if var_from_sos < -2^31 or var_from_sos > 2^31-1:
             return ['long', repr(var_from_sos)]    
     return ['int', repr(var_from_sos)]
 
-def _convert_bool_to_Java(var_from_sos):
+def _convert_bool_to_Java(self, var_from_sos, varName):
     return ['boolean','true'] if var_from_sos else ['boolean','false']
 
-def _convert_string_to_Java(var_from_sos):
+def _convert_string_to_Java(self, var_from_sos, varName):
     return ['String', '"'+var_from_sos+'"']
+
+def _convert_tuple_to_Java(self, var_from_sos, varName):
+    converter = _typeToConverterSwitch[type(var_from_sos[0])]
+    rawType = converter(self,var_from_sos[0], 'firstElement' )[0]
+    elementType = _Java_primitive_to_BoxingClass[rawType] if rawType in ('int', 'float', 'double', 'Byte') else rawType
+    homogenCollection = _check_type_homogeneity_in_collection(var_from_sos)
+    if not homogenCollection:
+        print(f'Java does not support collections (eg. Sets, Lists, Maps) with heterogenous types. Elements of {varName} will be saved as String in Java.') 
+        elementType = 'String'     
+    setInitString = f'new HashSet<{elementType}>(Arrays.asList('
+    for element in var_from_sos:
+        converter = _typeToConverterSwitch[type(element)]
+        elementValue = converter(self, element, varName )[1]
+        setInitString += f'{elementValue},'
+    setInitString = setInitString.rstrip(',')
+    setInitString += '))'
+    #converter = _typeToConverterSwitch[type(var_from_sos[0])]
+    return [f'HashSet<{elementType}>', setInitString]
 
 def _convert_float_to_Java(var_from_sos):
     # Checking for NaN, +/- infinity before returning actual value
@@ -30,20 +62,6 @@ def _convert_float_to_Java(var_from_sos):
     if var_from_sos == float('inf'):
         return ['Double', 'Double.POSITIVE_INFINITY']
     return ['double', repr(var_from_sos)]
-
-# Dict that maps python types to functions that will do the conversions
-_typeToConverterSwitch = {
-    None: _convert_None_to_Java,
-    bool: _convert_bool_to_Java,
-    np.bool: _convert_bool_to_Java,
-    int: _convert_Integers_to_Java,
-    np.int8: _convert_Integers_to_Java,
-    np.int16: _convert_Integers_to_Java,
-    np.int32: _convert_Integers_to_Java,
-    np.int64: _convert_Integers_to_Java,
-    str: _convert_string_to_Java,
-    float: _convert_float_to_Java
-}
 
 def _get_java_type_and_value(self, javaVar):
     try:
@@ -62,7 +80,6 @@ def _convert_from_java_to_Python(self, javaVar):
     if javaVarTypeAndValue["type"] in _javaStringTypes:
         return f'{javaVar} = "{javaVarTypeAndValue["value"]}"\n'
         
-
 def _convert_from_java_to_SoS(self, javaVar):
     javaVarTypeAndValue = _get_java_type_and_value(self, javaVar)
     if javaVarTypeAndValue["type"] in _javaNumericTypes:
@@ -81,14 +98,28 @@ def _readSettings():
     except Exception as exception:
             print(exception.__str__())
     return json.loads(settings, encoding='utf-8')
-        
+
+# Dict that maps python types to functions that will do the conversions
+_typeToConverterSwitch = {
+    None: _convert_None_to_Java,
+    bool: _convert_bool_to_Java,
+    np.bool: _convert_bool_to_Java,
+    int: _convert_Integers_to_Java,
+    np.int8: _convert_Integers_to_Java,
+    np.int16: _convert_Integers_to_Java,
+    np.int32: _convert_Integers_to_Java,
+    np.int64: _convert_Integers_to_Java,
+    str: _convert_string_to_Java,
+    float: _convert_float_to_Java,
+    tuple: _convert_tuple_to_Java
+}  
 class sos_java:
     settings = _readSettings()
     background_color = settings["color"] if settings else '#ffaba3'
     allow_overwrite = settings["allowOverwrite"] if settings else False
     supported_kernels = {'Java': ['java']}
-    #options = {'assignment_pattern': r'^\s*([A-Za-z0-9\.]+)\s*(=).*$'}
-    options = {}
+    options = {'assignment_pattern': r'^\s*([A-Za-z0-9\.]+)\s*(=).*$'}
+    #options = {}
     cd_command = ''
     __version__ = __version__
     java_vars = dict()
@@ -111,7 +142,7 @@ class sos_java:
             if changed:
                  self.sos_kernel.warn(f'Variable "{name}" from SoS to kernel {self.kernel_name} has been renamed to "{newname}" to follow language conventions')
             converter = _typeToConverterSwitch[type(env.sos_dict[name])]
-            type_and_value = converter(env.sos_dict[name]) if converter else None
+            type_and_value = converter(self, env.sos_dict[name], name) if converter else None
             if type_and_value is None:
                 self.sos_kernel.warn(f'Unsupported datatype {repr(env.sos_dict[name])} by {self.kernel_name}')
             else:
@@ -121,7 +152,7 @@ class sos_java:
                     self.sos_kernel.warn(f'Variable {name} is passed from SoS to kernel {self.kernel_name} already exists as {sos_java.java_vars[oldname]} type. Variable is saved as {newname}')
                 
                 result = self.sos_kernel.run_cell(
-                    f'{type_and_value[0]} {newname} = {type_and_value[1]}',
+                    f'{type_and_value[0]} {newname} = {type_and_value[1]};',
                     True,
                     False,
                     on_error=f'Failed to get variable {name} to Java, could not convert to Java type.') 
